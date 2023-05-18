70edfda7582fda61790361fd06ba4008ce2ea3cf
Atanas <flirtadjia.at@gmail.com>
2023-05-18 23:37:55 +0300

    1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+import org.example.first.Valleys;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Result is: "+new Valleys().getValleys("UDDDUUDU",8));
+    }
+}
---
+++ src/main/java/org/example/first/Valleys.java
@@ -0,0 +1,19 @@
+package org.example.first;
+public class Valleys {
+    public int getValleys(String path,int steps){
+        int count = 0;
+        int valleys=0;
+        for (int i = 0; i < steps; i++){
+            if((path.charAt(i)) == 'U'){
+                count += 1;
+                if (count==0){
+                    valleys+=1;
+                }
+            }else {
+                count -= 1;
+            }
+        }
+        return valleys;
+    }
+}
+
