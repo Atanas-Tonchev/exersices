14b9bda9d37b2fadf52f5a8affef14d4bee2ab26
Atanas <flirtadjia.at@gmail.com>
2023-05-18 23:39:38 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 import org.example.first.Valleys;
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Result is: "+new Valleys().getValleys("UDDDUUDU",8));
     }
 }
