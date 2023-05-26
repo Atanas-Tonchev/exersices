7589e10365e9f1f85b02a96d10df5724a91be3ad
Atanas-Tonchev <flirtadjia.at@gmail.com>
2023-05-26 08:59:23 +0300

    2.2

--- src/main/java/examples/Main.java
+++ src/main/java/examples/Main.java
@@ -1,13 +1,10 @@
 package examples;
-import examples.first.Valleys;
 import examples.second.SeatingStudents;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = {6,4};
-
-       // System.out.println("Result is: "+new Valleys().getValleys("UDDDUUDU",8));
-        System.out.println(new SeatingStudents().SeatingStudents(array));
+        int[] array = {8, 1, 8};
+        System.out.println(new SeatingStudents().seatingStudents(array));
     }
 }
--- src/main/java/examples/second/SeatingStudents.java
+++ src/main/java/examples/second/SeatingStudents.java
@@ -2,59 +2,23 @@ package examples.second;
 import java.util.ArrayList;
 import java.util.List;
 public class SeatingStudents {
-    public int SeatingStudents(int[] array) {
+    public int seatingStudents(int[] array) {
         int allDesk = array[0];
         int count = 0;
-        List<Integer> list = new ArrayList<>();
-        List<Integer> list1 = new ArrayList<>();
+        List<Integer> occupied = new ArrayList<>();
         for (int i = 1; i < array.length; i++) {
-            list1.add(array[i]);
+            occupied.add(array[i]);
         }
-        for (int j = 1; j <= allDesk; j++ ){
-            list.add(j);
-            for (Integer integer : list1){
-                list.remove(integer);
-            }
-        }
-        for (int k = 0; k < list.size(); k++){
-            int z = list.get(k);
-            int a = 0;
-            int b = 0;
-            if(z % 2 != 0) {
-               // System.out.println("1: "+list.get(k));
-                a = z;
-            }else {
-                b = z;
-                //System.out.println("2: "+list.get(k));
-            }
-            if (list.contains((a+2))) {
+        for (int j = 1; j < allDesk; j++ ){
+            int nextToEachDesk = j+1;
+            if(((j % 2) != 0) && (!occupied.contains(j)) && (!occupied.contains(nextToEachDesk))){
                 count++;
             }
-            if (list.contains((b+2))) {
+            int oneAfterAnotherDesk = j+2;
+            if(oneAfterAnotherDesk<allDesk && !occupied.contains(oneAfterAnotherDesk) && !occupied.contains(j)){
                 count++;
             }
-            if(list.contains((a+1))){
-                System.out.println("aaa: "+(a+1));
-                if((a+1)>1) {
-                    //count++;
-                }
-            }
-            if(list.contains((b+1))){
-                System.out.println("bbb: "+(b+1));
-                if((a+1)>1) {
-                   // count++;
-                }
-            }
-            /*if (list.contains() {
-                count++;
-            }*/
-
-            // System.out.println(b);
-            //System.out.println("3: "+sideDesk);
         }
-
-        System.out.println(list1);
-        System.out.println(list);
         return count;
     }
 }
