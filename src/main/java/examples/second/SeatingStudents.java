f35ebfd8fb5464702e8e4eac69d96567831f9428
Atanas-Tonchev <flirtadjia.at@gmail.com>
2023-05-26 09:00:44 +0300

    2.2

--- src/main/java/examples/second/SeatingStudents.java
+++ src/main/java/examples/second/SeatingStudents.java
@@ -5,17 +5,17 @@ public class SeatingStudents {
     public int seatingStudents(int[] array) {
         int allDesk = array[0];
         int count = 0;
-        List<Integer> occupied = new ArrayList<>();
+        List<Integer> occupiedDesks = new ArrayList<>();
         for (int i = 1; i < array.length; i++) {
-            occupied.add(array[i]);
+            occupiedDesks.add(array[i]);
         }
         for (int j = 1; j < allDesk; j++ ){
             int nextToEachDesk = j+1;
-            if(((j % 2) != 0) && (!occupied.contains(j)) && (!occupied.contains(nextToEachDesk))){
+            if(((j % 2) != 0) && (!occupiedDesks.contains(j)) && (!occupiedDesks.contains(nextToEachDesk))){
                 count++;
             }
             int oneAfterAnotherDesk = j+2;
-            if(oneAfterAnotherDesk<allDesk && !occupied.contains(oneAfterAnotherDesk) && !occupied.contains(j)){
+            if(oneAfterAnotherDesk<allDesk && !occupiedDesks.contains(oneAfterAnotherDesk) && !occupiedDesks.contains(j)){
                 count++;
             }
         }
