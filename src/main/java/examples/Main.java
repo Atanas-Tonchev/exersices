eb748e41d0f5b5c8c8825a782b4e39f12aedfbcb
Atanas-Tonchev <flirtadjia.at@gmail.com>
2023-05-26 15:52:40 +0300

    2.31

--- src/main/java/examples/Main.java
+++ src/main/java/examples/Main.java
@@ -4,7 +4,7 @@ import examples.tasks.NumberOfLetter;
 public class Main {
 
     public static void main(String[] args) {
-        String s = "a";
+        String s = "ab";
         System.out.println(new NumberOfLetter().numberOfLetter(s,1000000000));
     }
 }
--- src/main/java/examples/tasks/NumberOfLetter.java
+++ src/main/java/examples/tasks/NumberOfLetter.java
@@ -3,16 +3,17 @@ public class NumberOfLetter {
     public long numberOfLetter(String s,long n){
         int size = s.length();
         long count = 0;
-        long k = (n % size);
 
         for(int i = 0; i < size; i++) {
             if (s.charAt(i)=='a') {
                 count++;
             }
         }
+
         long a = (n / size);
         count = count * a;
 
+        long k = (n % size);
         for (int i = 0; i < k; i++) {
             if (s.charAt(i)=='a') {
                 count++;
