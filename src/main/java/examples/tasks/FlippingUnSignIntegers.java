c99afd66475bac04cb88be236c76f7ca59ef47b1
Atanas-Tonchev <flirtadjia.at@gmail.com>
2023-05-31 10:57:03 +0300

    2.4

--- src/main/java/examples/tasks/FlippingUnSignIntegers.java
+++ src/main/java/examples/tasks/FlippingUnSignIntegers.java
@@ -5,27 +5,25 @@ public class FlippingUnSignIntegers {
 
     public String getUnSignIntegers(int n, Scanner scanner) {
         List<Long> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             list.add(toBinaryFlip(scanner.nextInt()));
         }
         return list.stream().map(Object::toString).collect(Collectors.joining("\n"));
     }
-
     public static long toBinaryFlip(int x) {
         StringBuilder result = new StringBuilder();
         long unsignedIntMaxValue = Integer.toUnsignedLong(Integer.parseUnsignedInt("4294967295"));
         String maxValue = toBinary32Bit(unsignedIntMaxValue);
-        for (int i = 31; i >= 0 ; i--) {
+        for(int i = 31; i >= 0 ; i--) {
             int mask = 1 << i;
             result.append((x & mask) == 0 ? 1 : 0);
         }
-        if (!result.toString().equals(maxValue)) {
+        if(!result.toString().equals(maxValue)) {
             return unsignedIntMaxValue - x;
         }
 
             return unsignedIntMaxValue;
     }
-
     public static String toBinary32Bit(long x) {
         String result = Long.toBinaryString(x);
         return String.format("%32s", result).replaceAll(" ", "0");  // 32-bit Integer
