736b3a1c1533b0f49bcaba9eb753c29f649cfa97
Atanas-Tonchev <flirtadjia.at@gmail.com>
2023-05-31 11:33:33 +0300

    2.4

--- src/main/java/examples/tasks/FlippingUnSignIntegers.java
+++ src/main/java/examples/tasks/FlippingUnSignIntegers.java
@@ -8,22 +8,23 @@ public class FlippingUnSignIntegers {
         for(int i = 0; i < n; i++) {
             list.add(toBinaryFlip(scanner.nextInt()));
         }
+
         return list.stream().map(Object::toString).collect(Collectors.joining("\n"));
     }
     public static long toBinaryFlip(int x) {
         StringBuilder result = new StringBuilder();
-        long unsignedIntMaxValue = Integer.toUnsignedLong(Integer.parseUnsignedInt("4294967295"));
-        String maxValue = toBinary32Bit(unsignedIntMaxValue);
+        long unsignedMaxValue = Integer.toUnsignedLong(Integer.parseUnsignedInt("4294967295"));
+        String maxValue = toBinary32Bit(unsignedMaxValue);
         for(int i = 31; i >= 0 ; i--) {
             int mask = 1 << i;
             result.append((x & mask) == 0 ? 1 : 0);
         }
         if(!result.toString().equals(maxValue)) {
 
-            return unsignedIntMaxValue - x;
+            return unsignedMaxValue - x;
         }
 
-            return unsignedIntMaxValue;
+            return unsignedMaxValue;
     }
     public static String toBinary32Bit(long x) {
         String result = Long.toBinaryString(x);
