15124b33f5adac5e186c0e953c6dc4332917b230
Atanas-Tonchev <flirtadjia.at@gmail.com>
2023-06-07 12:51:22 +0300

    2.5

--- src/main/java/examples/Main.java
+++ src/main/java/examples/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please input numbers of queries: ");
         int q = scanner.nextInt();
--- src/main/java/examples/tasks/FlippingUnSignIntegers.java
+++ src/main/java/examples/tasks/FlippingUnSignIntegers.java
@@ -4,30 +4,12 @@ import java.util.stream.Collectors;
 public class FlippingUnSignIntegers {
 
     public String getUnSignIntegers(int n, Scanner scanner) {
+        long maxNum = 4294967295L;
         List<Long> list = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            list.add(toBinaryFlip(scanner.nextInt()));
+        for (int i = 0; i < n; i++) {
+            list.add(maxNum ^ scanner.nextInt());
         }
 
         return list.stream().map(Object::toString).collect(Collectors.joining("\n"));
-    }
-    public static long toBinaryFlip(int x) {
-        StringBuilder result = new StringBuilder();
-        long unsignedMaxValue = Integer.toUnsignedLong(Integer.parseUnsignedInt("4294967295"));
-        String maxValue = toBinary32Bit(unsignedMaxValue);
-        for(int i = 31; i >= 0 ; i--) {
-            int mask = 1 << i;
-            result.append((x & mask) == 0 ? 1 : 0);
-        }
-        if(!result.toString().equals(maxValue)) {
-
-            return unsignedMaxValue - x;
-        }
-
-        return unsignedMaxValue;
-    }
-    public static String toBinary32Bit(long x) {
-        String result = Long.toBinaryString(x);
-        return String.format("%32s", result).replaceAll(" ", "0");  // 32-bit Integer
     }
 }
